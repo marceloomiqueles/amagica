48c72e2a57fd101dbff60404dd88aef1e38f419b
marceloomiqueles <marcelomiqueles@hotmail.com>
2014-10-29 10:20:54 -0300

    Curl
    Se modificaron en ajax.php y crear_venta.php para trabajar con Curl y no
    con ajax, curl_test.php fue adaptado con 3 parametros extra 1 retorno de
    info y manejar respuesta en base a esa info

--- Test/ajax.php
+++ Test/ajax.php
@@ -25,55 +25,49 @@ if (isset($_POST["random"]) && isset($_POST["solicitud"])) {
             $hexakey = "";
             $hexakey = pasa_hexa($_POST["random"]);
             if (strlen($hexakey) == 64) {
-                // echo $hexakey;
                 $cliente->actualiza_random_licencia($_POST["random"], $_POST["solicitud"]);
                 $cliente->actualiza_token_licencia(md5($n_solicitud . md5($hexakey)), $_POST["solicitud"]);
                 echo $n_solicitud . "#" . md5($n_solicitud . md5($hexakey)) . "#" . $fecha_creacion . "#" . $fecha_fin . "#" . $lang . "#" . $curso;
-                ?>
-                <script type="text/javascript">
-                    loadDESCARGA('http://www.descargamagica.cl/acciones/modtoken.php', '<?php echo $curso; ?>', '<?php echo $n_solicitud; ?>', '<?php echo md5($n_solicitud . md5($hexakey)); ?>', '<?php echo $fecha_creacion; ?>', '<?php echo $fecha_fin; ?>', '<?php echo $lang; ?>');
-                    
-                    function loadDESCARGA(php_file, tipolicencia, licencia, token, fechainicio, fechatermino, idioma) {
-                        alert("Estamos generando su descarga. Por favor espere y no cierre la ventana hasta que se le indique que el proceso ha finalizado. Gracias"); //O colcale un gif animado de preload
-                        var datastring= "tipolicencia="+tipolicencia+"&licencia="+licencia+"&token="+token+"&fechainicio="+fechainicio"&fechatermino="+fechatermino"&idioma="+idioma;
-                        var urlfile = php_file + "?x=" + parseInt(Math.random() * 1000000);
-                        var request =  get_XmlHttpx();
-                        request.open("POST", urlfile , true);
-                        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                        request.setRequestHeader("Content-length", datastring.length);
-                        request.send(datastring);
-                        request.onreadystatechange = function() {
-                            //alert(request.readyState);
-                            if (request.readyState == 4) { //respuesta ok
-                                //alert(request.responseText);
-                            }
-                        }
-                    }
-
-                    //Request multiplataforma
-                    function get_XmlHttpx() {
-                        var xmlHttp = null;
-                        try{
-                            // Opera 8.0+, Firefox, Safari
-                            xmlHttp = new XMLHttpRequest();
-                        } catch (e) {
-                            // Internet Explorer Browsers
-                            try{
-                                xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
-                            } catch (e) {
-                                try{
-                                    xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-                                } catch (e) {
-                                    // Something went wrong
-                                    alert("AJAX no soportado");
-                                    return false;
-                                }
-                            }
-                        }
-                        return xmlHttp;
-                    }
-                </script>
-                <?php
+                // Extract data from the post
+                extract($_POST);
+                // Set POST variables
+                $url = 'http://www.descargamagica.cl/acciones/modtoken.php';
+                $fields = array(
+                    'tipolicencia'=>urlencode($curso),
+                    'licencia'=>urlencode($n_solicitud),
+                    'token'=>urlencode(md5($n_solicitud . md5($hexakey))),
+                    'fechainicio'=>urlencode($fecha_creacion),
+                    'fechatermino'=>urlencode($fecha_fin),
+                    'idioma'=>urlencode($lang)
+                );
+                // url-ify the data for the POST
+                foreach($fields as $key=>$value) {
+                    $fields_string .= $key . '=' . $value . '&'; 
+                }
+                rtrim($fields_string,'&');
+                // Open connection
+                $ch = curl_init();
+                // Set the url, number of POST vars, POST data
+                curl_setopt($ch,CURLOPT_URL,$url); 
+                curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+                curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+                curl_setopt($ch,CURLOPT_POST,count($fields));
+                curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
+                // Execute post
+                $result = curl_exec($ch);
+                // Get info post
+                $info = curl_getinfo($ch);
+                // Close connection
+                curl_close($ch);
+                if (empty($result)) {
+                    exit();
+                }
+                if (empty($info['http_code'])) {
+                    exit();
+                } else {
+                    $curlok = $info['http_code'];
+                }
             }
         }
 
--- Test/curl_test.php
+++ Test/curl_test.php
@@ -1,34 +1,41 @@
 <?php 
-//extract data from the post
+// extract data from the post
 extract($_POST);
 
-//set POST variables
-$url = 'http://domain.com/get-post.php';
+// set POST variables
+$url = 'http://www.descargamagica.cl/acciones/modtoken.php';
 $fields = array(
-                        'lname'=>urlencode($last_name),
-                        'fname'=>urlencode($first_name),
-                        'title'=>urlencode($title),
-                        'company'=>urlencode($institution),
-                        'age'=>urlencode($age),
-                        'email'=>urlencode($email),
-                        'phone'=>urlencode($phone)
-                );
+    'tipolicencia'=>urlencode($curso),
+    'licencia'=>urlencode($n_solicitud),
+    'token'=>urlencode(md5($n_solicitud . md5($hexakey))),
+    'fechainicio'=>urlencode($fecha_creacion),
+    'fechatermino'=>urlencode($fecha_fin),
+    'idioma'=>urlencode($lang)
+);
 
-//url-ify the data for the POST
-foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+// url-ify the data for the POST
+foreach($fields as $key=>$value) {
+	$fields_string .= $key . '=' . $value . '&'; 
+}
 rtrim($fields_string,'&');
 
-//open connection
+// open connection
 $ch = curl_init();
 
-//set the url, number of POST vars, POST data
-curl_setopt($ch,CURLOPT_URL,$url);
+// set the url, number of POST vars, POST data
+curl_setopt($ch,CURLOPT_URL,$url); 
+curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 curl_setopt($ch,CURLOPT_POST,count($fields));
 curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
 
-//execute post
+// execute post
 $result = curl_exec($ch);
 
-//close connection
+// get info post
+$info = curl_getinfo($ch);
+
+// close connection
 curl_close($ch);
 ?>
